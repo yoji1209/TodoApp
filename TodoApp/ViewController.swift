486fc1f38089b21a45e73757875e71048fd0032e
yoji1209 <eikouyou529@gmail.com>
2025-09-11 12:03:00 +0900

    Add Xcode TodoApp project

---
+++ TodoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoApp
+//
+//  Created by Yoji Akaishi on 2025/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoAppTests/TodoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppTests.swift
+//  TodoAppTests
+//
+//  Created by Yoji Akaishi on 2025/09/11.
+//
+
+import Testing
+@testable import TodoApp
+
+struct TodoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
